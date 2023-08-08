142c1c8b2d1e5ee52a93f06bf975401e7fafd819
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-08-07 17:42:52 -0700

    finished event controllers and started stage controllers

---
+++ controllers/stages_controller.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES
+const stages = require('express').Router()
+const db = require('../models')
+const { Stage } = db 
+const { Op } = require('sequelize')
+
+// Find all stages
+stages.get('/', async (req, res) => {
+    try {
+        const foundStages = await Stage.findAll({
+            where: {
+                stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}%` }
+            }
+        })
+        res.status(200).json(foundStages)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
